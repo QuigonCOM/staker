6cbde10dea72bf39530fce310353c919a5dd07e8
rupansh <rupanshsekar@hotmail.com>
2021-10-17 11:00:54 +0000

    Build: (4145444) add workflow for automatic typings build

---
+++ dist/index.d.ts
@@ -0,0 +1,4 @@
+export type { XpNet } from "./XpNet";
+export type { XpNetStaker } from "./XpNetStaker";
+export { XpNet__factory } from "./factories/XpNet__factory";
+export { XpNetStaker__factory } from "./factories/XpNetStaker__factory";
---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.XpNetStaker__factory = exports.XpNet__factory = void 0;
+var XpNet__factory_1 = require("./factories/XpNet__factory");
+Object.defineProperty(exports, "XpNet__factory", { enumerable: true, get: function () { return XpNet__factory_1.XpNet__factory; } });
+var XpNetStaker__factory_1 = require("./factories/XpNetStaker__factory");
+Object.defineProperty(exports, "XpNetStaker__factory", { enumerable: true, get: function () { return XpNetStaker__factory_1.XpNetStaker__factory; } });
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi9zcmMvaW5kZXgudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7O0FBTUEsNkRBQTREO0FBQW5ELGdIQUFBLGNBQWMsT0FBQTtBQUN2Qix5RUFBd0U7QUFBL0QsNEhBQUEsb0JBQW9CLE9BQUEifQ==
